44f53c7dc5275871f39e807646ea575a97250d10
valeriomatranga <matrangav@gmail.com>
2021-04-26 16:34:12 +0200

    ciclo array

---
+++ asset/js/main.js
@@ -0,0 +1,13 @@
+//chiedere all'utente la sua mail
+var mail = prompt ("Scrivi la tua Mail");
+console.log(mail);
+
+//lista delle mail che possono accedere
+var lista = ["vale","fili","marco","lori"];
+
+//verifica accesso mail
+for (var mail = 0; mail < lista.length; mail++) {
+    console.log(lista);
+}
+
+
